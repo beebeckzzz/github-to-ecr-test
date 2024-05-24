677de9519db110aeefab0da3d7f3b4ba51ad94b5
beebeckzzz <beebeckzzz@gmail.com>
2024-05-25 00:17:30 +0545

    Update app.py

--- scripts/ecr-image/app.py
+++ scripts/ecr-image/app.py
@@ -3,5 +3,5 @@ def lambda_handler(event, context):
     print("Hello from the Lambda function!")
     return {
         "statusCode": 200, 
-        "body": "Hello from Lambda! Modified."
+        "body": "Hello from Lambda! Modified!"
         }
